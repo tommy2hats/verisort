0a532ee2812435e2c5ff0769ee69a917b318c945
Shoaib Shaikh <shoaibshaikh07@yahoo.com>
2026-05-29 06:57:58 -0400

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,24 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	return [
-		{
-			url: "https://verisortai.com",
-			lastModified: new Date(),
-			changeFrequency: "daily",
-			priority: 1,
-		},
-	];
+  return [
+    {
+      url: "https://verisortai.com",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://verisortai.com/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://verisortai.com/terms",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
 }
